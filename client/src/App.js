1fe960a514a26243829d988b676772423b4c7c02
Andrei Nascimento <andrei.lima.nas@outlook.com>
2023-03-25 13:26:55 -0300

    Inclusão do histórico de lançamento dos dados

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,20 @@ import "./App.css";
 function App() {
   const [ladosDado, setLadosDado] = useState(6);
   const [resultado, setResultado] = useState(null);
+  const [historicoLancamentos, setHistoricoLancamentos] = useState([]);
 
   function handleSelecionarDado(lados) { 
     axios.get(`rolar/${lados}`).then(function(res) {
       const resultado = res.data.resultado;
       setResultado(`O resultado da rolagem foi: ${resultado}`);
+      setHistoricoLancamentos([...historicoLancamentos, resultado]);
     }).catch(function(error) {
       console.error(error);
     });
+  }
+
+  function handleLimparHistorico() {
+    setHistoricoLancamentos([]);
   }
 
   return (
@@ -30,6 +36,20 @@ function App() {
         <p className="lados">Dado de <strong>{ladosDado} lados</strong> selecionado</p>
 
         {resultado && <p className="resultado">{resultado}</p>} 
+        
+        <div className="boxHistorico">
+          {historicoLancamentos.length > 0 && (
+            <div className="boxNumeros">
+              <p className="titleHistorico">Histórico de lançamentos:</p>
+              <ul>
+                {historicoLancamentos.map((resultado, index) => (
+                  <li key={index}>{resultado}-</li>
+                ))}
+              </ul>
+            </div>
+          )}
+          <button className="btnLimpar" onClick={handleLimparHistorico}>Limpar histórico</button>
+        </div>
         
       </div>
     </div>
